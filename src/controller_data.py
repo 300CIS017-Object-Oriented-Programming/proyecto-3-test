eeffbd397aa64114edf8c151af4da8c7301f357e
Daniel Posada <daniel.lejandr0o0@gmail.com>
2024-11-24 22:43:27 -0500

    NEW - Se implementó el método para generar el consolidado.

--- src/controller_data.py
+++ src/controller_data.py
@@ -3,12 +3,14 @@ from gestor_archivos import gestor_archivos, obtener_type_year
 from collections import defaultdict
 import pandas as pd
 
+
 class controller_data:
     def __init__(self):
         self.__gestores_datos = [gestor_xlsx(), gestor_csv(), gestor_json()]
         self.__programas_data = pd.DataFrame()
         self.__gestor_archivos = gestor_archivos()
         self.__data_sets = defaultdict(dict)
+        self.__consolidado = pd.DataFrame()
         self.__data_frame_filtrado = pd.DataFrame()
 
     def importar_datos(self, archivo):
@@ -33,14 +35,59 @@ class controller_data:
             extension = "csv"
         gestor.exportar_datos(self.__data_frame_filtrado, f"../docs/outputs/data_filtrado." + extension)
 
-    def filtrar_datos(self, tipo, anios, keyword):
-        for anio in anios:
-            data_frame = self.__data_sets[tipo][anio]
-            if 'PROGRAMA_ACADEMICO' in data_frame.columns:
-                # Filtra las filas donde la columna PROGRAMA_ACADEMICO contiene la palabra clave
-                filas_filtradas = data_frame[
-                    data_frame['PROGRAMA_ACADEMICO'].str.contains(keyword, case=False, na=False)]
-                # Agrega una columna con el año
-                filas_filtradas['AÑO'] = anio
-                # Agrega las filas filtradas al data_frame_filtrado
-                self.__data_frame_filtrado = pd.concat([self.__data_frame_filtrado, filas_filtradas])
+    def filtrar_datos(self, anios, keyword):
+        programas_agregados = set()
+        for tipo in self.__data_sets:
+            for anio in anios:
+                data_frame = self.__data_sets[tipo][anio]
+                if 'PROGRAMA_ACADEMICO' in data_frame.columns:
+                    filas_filtradas = data_frame[
+                        data_frame['PROGRAMA_ACADEMICO'].str.contains(keyword, case=False, na=False)]
+                    for _, fila in filas_filtradas.iterrows():
+                        programa = fila['PROGRAMA_ACADEMICO']
+                        if programa not in programas_agregados:
+                            programas_agregados.add(programa)
+                            fila['AÑO'] = anio
+                            self.__data_frame_filtrado = pd.concat([self.__data_frame_filtrado, fila.to_frame().T])
+
+    def _obtener_primer_valor(self, columna, programa):
+        df_programa_filtrado = self.__data_frame_filtrado[
+            self.__data_frame_filtrado['PROGRAMA_ACADEMICO'] == programa]
+
+        if not df_programa_filtrado.empty and columna in df_programa_filtrado.columns:
+            return df_programa_filtrado[columna].iloc[0]
+        return None
+
+    def generar_consolidado(self, anios):
+        consolidado = pd.DataFrame()
+
+        for programa in self.__data_frame_filtrado['PROGRAMA_ACADEMICO'].unique():
+            fila = {
+                'PROGRAMA_ACADEMICO': programa,
+                'MODALIDAD': self._obtener_primer_valor('MODALIDAD', programa),
+                'ID_SEXO': self._obtener_primer_valor('ID_SEXO', programa),
+                'SEXO': self._obtener_primer_valor('SEXO', programa),
+                'NIVEL_FORMACION': self._obtener_primer_valor('NIVEL_FORMACION', programa),
+            }
+
+            for tipo in self.__data_sets.keys():
+                for anio in anios:
+                    if anio in self.__data_sets[tipo]:
+                        df_tipo_anio = self.__data_sets[tipo][anio]
+                        df_tipo_anio_programa = df_tipo_anio[df_tipo_anio['PROGRAMA_ACADEMICO'] == programa]
+                        fila[f'{tipo.upper()}_{anio}'] = df_tipo_anio_programa[tipo.upper()]
+                    else:
+                        fila[f'{tipo.upper()}_{anio}'] = 0
+
+            consolidado = pd.concat([consolidado, pd.DataFrame([fila])], ignore_index=True)
+
+        self.__consolidado = consolidado
+
+
+
+
+
+
+
+
+
